4cebda2e3d50db86bd95cce0bd14a1d2a9b3cb26
atkmehmet <durmazmehmet47@gmail.com>
2024-01-30 00:22:01 +0300

    Add your command

--- app/src/main/java/com/example/tabmenuex/representation/mainScreen.kt
+++ app/src/main/java/com/example/tabmenuex/representation/mainScreen.kt
@@ -6,23 +6,70 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Face
+import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 
 import androidx.compose.runtime.Composable
+
+import  androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.tabmenuex.Model.Bottom
 
 @Composable
 fun Screen(){
-
+ val menu= listOf(
+     Bottom(
+         title = "Home",
+         selectedIcon = Icons.Default.Home,
+         unSelectedIcon = Icons.Outlined.Home,
+         hasBadge = false,
+         badgeNum = 0
+     ),
+     Bottom(
+         title = "Inbox",
+         selectedIcon = Icons.Default.Email,
+         unSelectedIcon = Icons.Outlined.Email,
+        hasBadge = true,
+        badgeNum = 10
+ ),
+     Bottom(
+         title = "Account",
+         selectedIcon = Icons.Default.Face,
+         unSelectedIcon = Icons.Outlined.Face,
+         hasBadge = true,
+         badgeNum = 0
+     )
+ )
+    var bottomNavState by rememberSaveable{ mutableStateOf(0) }
     Scaffold(
-        bottomBar = NavigationBar {
-            NavigationBarItem(selected = , onClick = { /*TODO*/ }, icon = { /*TODO*/ })
+        bottomBar = {
+            NavigationBar {
+                menu.forEachIndexed { index, bottom ->
+
+                    NavigationBarItem(
+                        selected = bottomNavState == index,
+                        onClick = { bottomNavState = index },
+                        icon = {
+                            if (bottomNavState == index) bottom.selectedIcon else bottom.unSelectedIcon
+                        })
+
+                }
+            }
         }
-    ) {
+    )
+
+    {
        contentPadding->
         Column (
             modifier = Modifier
@@ -32,7 +79,7 @@ fun Screen(){
             horizontalAlignment = Alignment.CenterHorizontally
         ){
                 Text(
-                    text = "content",
+                    text = menu[bottomNavState].toString(),
                     fontWeight = FontWeight.Bold,
                     fontSize = 44.sp
                 )
